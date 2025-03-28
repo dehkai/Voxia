8216ba58bc4e57d7a512cdfb1c81d7a70e8ff85a
Yap Deh Kai <ydk1421@gmail.com>
2025-03-28 16:12:38 +0800

    Integrate frontend with sentry for error tracking and monitoring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
